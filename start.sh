c5c1ed4981fe1d5916dcac5c53d25e8eaac04193
Xiaolu Hong <xiaoluhong@rancher.com>
2019-03-05 13:15:51 +0800

    Create start.sh

---
+++ start.sh
@@ -0,0 +1,9 @@
+#!/bin/bash  
+
+src=/etc/webhook/source
+dest=/etc/webhook
+ 
+/usr/local/bin/inotifywait -mrq -e modify,delete,create,attrib $src |  while read file 
+do  
+      cat $src/* | jq -s add > $dest/hooks.json 
+done
