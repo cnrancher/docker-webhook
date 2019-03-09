24a1e8460a786ef0e32aed7c9bec0e50564fc756
Xiaolu Hong <xiaoluhong@rancher.com>
2019-03-09 21:49:45 +0800

    Update monitoring.sh

--- monitoring.sh
+++ monitoring.sh
@@ -2,7 +2,9 @@
 
 src=/etc/webhook/source
 dest=/etc/webhook
- 
+
+cat $src/* | jq -s add > $dest/hooks.json 
+
 inotifywait -mrq -e modify,delete,create,attrib $src |  while read file 
 do  
       cat $src/* | jq -s add > $dest/hooks.json 
